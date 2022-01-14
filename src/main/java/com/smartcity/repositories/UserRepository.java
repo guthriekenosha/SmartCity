cdab525eb54e68305768e4f75c2a2dc42510423b
Kenosha Guthrie <kenoshaguthrie@gmail.com>
2022-01-13 21:42:08 -0600

    repo

--- src/main/java/com/smartcity/repositories/UserRepository.java
+++ src/main/java/com/smartcity/repositories/UserRepository.java
@@ -1,5 +1,12 @@
 package com.smartcity.repositories;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.smartcity.models.User;
+
+public interface UserRepository  extends CrudRepository<User, Long> {
+	
+	User findbyEmail(String email);
+	
 
 }
--- src/main/java/com/smartcity/services/UserService.java
+++ src/main/java/com/smartcity/services/UserService.java
@@ -1,5 +1,42 @@
 package com.smartcity.services;
 
+import com.smartcity.repositories.UserRepository;
+
 public class UserService {
+	
+	private UserRepository userRepo;
+	
+	
+
+	public UserService(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	
+	}
+	
+	
+	
+	
+	//Register User
+	
+	
+	
+	
+	
+	//Find User Email
+	
+	
+	
+	
+	//Find User by Id
+	
+	
+	
+	//authenticate user
+	
+	
+	
+	
+	
+	
 
 }
